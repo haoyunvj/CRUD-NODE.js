93123b588bc94717eb94723359d3e601bf1d9b64
haoyunvj <jvwasdead@icloud.com>
2025-01-21 22:18:01 -0300

    Segundo commit

--- src/app.js
+++ src/app.js
@@ -13,6 +13,16 @@ const selecoes = [
     {id: 4, selecao: 'Camarões', grupo: 'G'},
 ]
 
+// retornar o objeto por id
+function buscarSelecaoPorId(id) {
+    return selecoes.filter( selecao => selecao.id == id)
+}
+
+// pegar a posição ou index do elemento no array por id
+function buscaIndexSelecao(id) {
+    return selecoes.findIndex( selecao => selecao.id == id)
+}
+
 /*criar rota padrão ou raiz*/
 
 app.get('/', (req, res) => {
@@ -23,9 +33,26 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscaIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso!!`)
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscaIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
 })
 
 export default app
