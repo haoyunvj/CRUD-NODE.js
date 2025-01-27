ff83fa425a5b444f7d968da2a795f29df4b7832c
haoyunvj <jvwasdead@icloud.com>
2025-01-26 23:50:37 -0300

    Terceiro commit: conexão com db MySQL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import app from './src/app.js';
+
+import conexao from './infra/conexao.js';
+
 const PORT = 3000
 
-// escutar a porta 3000
+// fazer a conexão
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log(`Conexão realizada com sucesso`)
+        // escutar a porta 3000
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+        app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`)
 })
+    }
+})
+
